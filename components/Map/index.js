09335023193e6cfd977fb9aa0b28f9eedfd3cf4d
Paul Kerwin Acero <akosipoloyacero@gmail.com>
2021-02-19 08:41:19 +0400

    update on hooks currentLocation

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import GoogleMapReact from 'google-map-react';
+import currentLocation from '../../hooks/currentLocation';
+import watchLocation from '../../hooks/watchLocation';
+import Marker from '../Marker'
+import styles from '../../styles/Home.module.css'
 
 const getInfoWindowString = (place) => `
     <div>
@@ -17,7 +21,7 @@ const handleApiLoaded = (map, maps, places) => {
   const markers = [];
   const infowindows = [];
 
-  places.forEach((place) => {
+  for(const place of places) {
     markers.push(new maps.Marker({
       position: {
         lat: place.latitude,
@@ -29,7 +33,7 @@ const handleApiLoaded = (map, maps, places) => {
     infowindows.push(new maps.InfoWindow({
       content: getInfoWindowString(place),
     }));
-  });
+  };
 
   markers.forEach((marker, i) => {
     marker.addListener('click', () => {
@@ -43,31 +47,34 @@ const Wrapper = styled.main`
   height: 500px;
 `;
 
-const Map = ({ children, ...props }) => (
-  <Wrapper>
-    <GoogleMapReact
-      // for testing purpose only, add this API key in a environment variable
-      defaultZoom={6}
-      bootstrapURLKeys={{
-        key: 'AIzaSyDs7RrfV4rRk26YtCFDgH0AxM-OP3T_GQI',
-      }}
-      yesIWantToUseGoogleMapApiInternals
-      onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps, props.places)}
-      {...props}
-    >
-      {children}
-    </GoogleMapReact>
-    {props.places.map((e, index) => (
-        <div key={index}>
-            <div>
+const Map = ({ children, ...props }) => {
+
+  return (
+    <Wrapper>
+      <GoogleMapReact
+        // for testing purpose only, add this API key in a environment variable
+        defaultZoom={6}
+        bootstrapURLKeys={{
+          key: '',
+        }}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) => handleApiLoaded(map, maps, props.places)}
+        {...props}
+      >
+
+        {children}
+      </GoogleMapReact>
+      {props.places.map((e, index) => (
+          <div key={index}>
+            <div className={styles.card} style={{cursor: 'pointer'}} onClick={() => {console.log('test')}}>
               <span>Name: {e.name}</span><br/>
               <span>Address: {e.address}</span><br/>
-              <span>Distance: </span>
             </div>
-        </div>
-      ))}
-  </Wrapper>
-);
+          </div>
+        ))}
+    </Wrapper>
+  );
+}
 
 Map.propTypes = {
   children: PropTypes.oneOfType([
--- pages/restaurants.js
+++ pages/restaurants.js
@@ -1,15 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Map from '../components/Map';
+import Marker from '../components/Marker';
 
 export default function Restaurants({ restaurants }) {
+  
   return (
     <div>
       <Map
         defaultZoom={11}
         defaultCenter={[25.204849, 55.270782]}
         places={restaurants}
-      />
+      >
+        
+      </Map>
     </div>
   );
 }
